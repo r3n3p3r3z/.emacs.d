cdd6c0973bafb42336f1302045b9a3c7f6971e67
Alex <alexander.soto@gmail.com>
2019-05-01 22:18:10 -0400

    pushed personal emacs configuration

--- init.el
+++ init.el
@@ -1,30 +1,494 @@
-;;; Package --- Summary
+(eval-and-compile
+  (setq gc-cons-threshold 402653184
+        gc-cons-percentage 0.6))
 
-;;; Commentary:
-;; Emacs init file responsible for either loading a pre-compiled configuration file
-;; or tangling and loading a literate org configuration file.
 
-;;; Code:
+(require 'org)
 
-;; Don't attempt to find/apply special file handlers to files loaded during startup.
-(let ((file-name-handler-alist nil))
-  ;; If config is pre-compiled, then load that
-  (if (file-exists-p (expand-file-name "emacs.elc" user-emacs-directory))
-      (load-file (expand-file-name "emacs.elc" user-emacs-directory))
-    ;; Otherwise use org-babel to tangle and load the configuration
-    (require 'org)
-    (org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))))
 
-;;; init.el ends here
+
+
+;; package: Emacs Package Manager
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/package.org"
+		      user-emacs-directory))
+
+;; aesthetics: My taste
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/aesthetics.org"
+		      user-emacs-directory))
+
+
+;; hyperbole: a programmable hypertextual information management system.
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/hyperbole.org"
+		      user-emacs-directory))
+
+
+
+;;org: The real reason to use emacs
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/org.org"
+		      user-emacs-directory))
+
+
+
+
+;; helm: Indescribably awesome
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/helm.org"
+		      user-emacs-directory))
+
+;; Ivy
+;; ;; ------------------------------------------------------------------------
+;; (org-babel-load-file (expand-file-name
+;; 		      "~/.emacs.d/modules/ivy.org"
+;; 		      user-emacs-directory))
+
+;; ;; Counsel
+;; ;; ------------------------------------------------------------------------
+;; (org-babel-load-file (expand-file-name
+;; 		      "~/.emacs.d/modules/counsel.org"
+;; 		      user-emacs-directory))
+
+
+
+
+
+;; company: Auto complete anything, anywhere
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/company.org"
+		      user-emacs-directory))
+
+;; yasnippet: Snippet and be done with it
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/yasnippet.org"
+		      user-emacs-directory))
+
+
+;; flyspell: Get the computer proof reading
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/flyspell.org"
+		      user-emacs-directory))
+
+;; flycheck: Get the computer proof reading
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/flycheck.org"
+		      user-emacs-directory))
+
+;; which-key: Display available keybindings in popup
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/which-key.org"
+		      user-emacs-directory))
+
+;; Ranger: a multi-view column display
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/ranger.org"
+		      user-emacs-directory))
+
+;; Howm: a wiki-like note-taknig tool
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/howm.org"
+		      user-emacs-directory))
+
+
+;; general: Convenience wrappers for keybindings
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/general.org"
+		      user-emacs-directory))
+
+;; org-pomodoro:
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/org-pomodoro.org"
+		      user-emacs-directory))
+
+;; Olivetti: nice writing environment
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/olivetti.org"
+		      user-emacs-directory))
+
+;; ;; EMMS:
+;; ;; ------------------------------------------------------------------------
+;; (org-babel-load-file (expand-file-name
+;; 		      "~/.emacs.d/modules/emms.org"
+;; 		      user-emacs-directory))
+
+;; pdf-tools:
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/pdf-tools.org"
+		      user-emacs-directory))
+
+;; org-cliplink
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/org-cliplink.org"
+		      user-emacs-directory))
+
+;; link-hint
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/link-hint.org"
+		      user-emacs-directory))
+
+;; markdown-mode
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/markdown-mode.org"
+		      user-emacs-directory))
+
+;;telephone-line
+;;------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/telephone-line.org"
+		      user-emacs-directory))
+
+;; helpful
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/helpful.org"
+		      user-emacs-directory))
+
+;; deft
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/deft.org"
+		      user-emacs-directory))
+
+;; magit
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/magit.org"
+		      user-emacs-directory))
+
+;; elfeed
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/elfeed.org"
+		      user-emacs-directory))
+
+;; avy
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/avy.org"
+		      user-emacs-directory))
+;; org-noter
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/org-noter.org"
+		      user-emacs-directory))
+
+;; org-clock
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/org-clock.org"
+		      user-emacs-directory))
+
+;; dired
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/dired.org"
+		      user-emacs-directory))
+
+;; delight
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/delight.org"
+		      user-emacs-directory))
+
+;; async
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/async.org"
+		      user-emacs-directory))
+
+;; projectile
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/projectile.org"
+		      user-emacs-directory))
+
+;; org-journal
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/org-journal.org"
+		      user-emacs-directory))
+
+;; alert
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/alert.org"
+		      user-emacs-directory))
+
+;; helm-org-rifle
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/helm-org-rifle.org"
+		      user-emacs-directory))
+
+;; anzu
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/anzu.org"
+		      user-emacs-directory))
+
+;; org-brain
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/org-brain.org"
+		      user-emacs-directory))
+
+;; org-download
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/org-download.org"
+		      user-emacs-directory))
+
+;; wiki-summary
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/wiki-summary.org"
+		      user-emacs-directory))
+
+;; ace-window
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/ace-window.org"
+		      user-emacs-directory))
+
+;; org-web-tools
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/org-web-tools.org"
+		      user-emacs-directory))
+
+;; solarized-theme
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/solarized-theme.org"
+		      user-emacs-directory))
+
+;; hydra
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/hydra.org"
+		      user-emacs-directory))
+
+;; secretaria
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/secretaria.org"
+		      user-emacs-directory))
+
+;; swiper
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/swiper.org"
+		      user-emacs-directory))
+
+;; persistent scratch
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/persistent-scratch.org"
+		      user-emacs-directory))
+
+
+;; scratch buffers
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/scratch-buffers.org"
+		      user-emacs-directory))
+
+;; notdeft
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/notdeft.org"
+		      user-emacs-directory))
+
+
+;; nov
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/nov.org"
+		      user-emacs-directory))
+
+;; zenburn
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/zenburn.org"
+		      user-emacs-directory))
+
+
+;; zetteldeft
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/zetteldeft.org"
+		      user-emacs-directory))
+
+
+
+
+
+
+
+
+
+;; ;; Start of activities
+
+;; programming: Minor modes and tweaks
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/activities/programming.org"
+		      user-emacs-directory))
+
+
+;; journal system:
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/activities/journal.org"
+		      user-emacs-directory))
+
+;; memacs:
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/activities/memacs.org"
+		      user-emacs-directory))
+
+;; org-agenda
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/activities/org-agenda.org"
+		      user-emacs-directory))
+
+;; encryption
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/activities/encryption.org"
+		      user-emacs-directory))
+
+;; GTD
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/activities/gtd.org"
+		      user-emacs-directory))
+
+;; Misc
+;; ------------------------------------------------------------------------
+(org-babel-load-file (expand-file-name
+		      "~/.emacs.d/modules/activities/misc.org"
+		      user-emacs-directory))
+
+
+
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(alert-default-style 'libnotify)
+ '(ansi-color-names-vector
+   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
+ '(company-begin-commands '(self-insert-command))
+ '(company-idle-delay 0.2)
+ '(company-minimum-prefix-length 0)
+ '(company-show-numbers t)
+ '(company-tooltip-align-annotations t)
+ '(compilation-message-face 'default)
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#657b83")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
  '(custom-safe-themes
-   '("151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "cd736a63aa586be066d5a1f0e51179239fe70e16a9f18991f6f5d99732cabb32" "4697a2d4afca3f5ed4fdf5f715e36a6cac5c6154e105f3596b44a4874ae52c45" "75d3dde259ce79660bac8e9e237b55674b910b470f313cdf4b019230d01a982a" "7e78a1030293619094ea6ae80a7579a562068087080e01c2b8b503b27900165c" "8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "88049c35e4a6cedd4437ff6b093230b687d8a1fb65408ef17bfcf9b7338734f6" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))
- '(org-journal-file-format "%Y.%m.%d.org")
- '(package-selected-packages '(org-plus-contrib projectile use-package)))
+   '("04232a0bfc50eac64c12471607090ecac9d7fd2d79e388f8543d1c5439ed81f5" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))
+ '(dired-auto-revert-buffer t)
+ '(dired-dwim-target t)
+ '(dired-hide-details-hide-symlink-targets nil)
+ '(dired-listing-switches "-alh")
+ '(dired-ls-F-marks-symlinks nil)
+ '(dired-recursive-copies 'always)
+ '(fci-rule-color "#eee8d5")
+ '(highlight-changes-colors '("#d33682" "#6c71c4"))
+ '(highlight-symbol-colors
+   (--map
+    (solarized-color-blend it "#fdf6e3" 0.25)
+    '("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2")))
+ '(highlight-symbol-foreground-color "#586e75")
+ '(highlight-tail-colors
+   '(("#eee8d5" . 0)
+     ("#B4C342" . 20)
+     ("#69CABF" . 30)
+     ("#69B7F0" . 50)
+     ("#DEB542" . 60)
+     ("#F2804F" . 70)
+     ("#F771AC" . 85)
+     ("#eee8d5" . 100)))
+ '(hl-bg-colors
+   '("#DEB542" "#F2804F" "#FF6E64" "#F771AC" "#9EA0E5" "#69B7F0" "#69CABF" "#B4C342"))
+ '(hl-fg-colors
+   '("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3"))
+ '(hl-paren-colors '("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900"))
+ '(magit-diff-use-overlays nil)
+ '(nrepl-message-colors
+   '("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4"))
+ '(org-clock-clocktable-default-properties
+   '(:block day :maxlevel 6 :scope agenda :link nil :compact t :formula % :step day :fileskip0 t :stepskip0 t :narrow 80 :properties
+            ("TODO")))
+ '(org-clock-continuously t)
+ '(org-journal-dir "~/org/archive/2019/04/")
+ '(org-journal-file-format "%Y-%m-%d.org")
+ '(org-show-notification-handler '(lambda (msg) (alert msg)))
+ '(package-selected-packages
+   '(anaconda-mode yaml-mode flycheck yasnippet use-package helm company))
+ '(pos-tip-background-color "#eee8d5")
+ '(pos-tip-foreground-color "#586e75")
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
+ '(term-default-bg-color "#fdf6e3")
+ '(term-default-fg-color "#657b83")
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   '((20 . "#dc322f")
+     (40 . "#c8805d801780")
+     (60 . "#bec073400bc0")
+     (80 . "#b58900")
+     (100 . "#a5008e550000")
+     (120 . "#9d0091000000")
+     (140 . "#950093aa0000")
+     (160 . "#8d0096550000")
+     (180 . "#859900")
+     (200 . "#66aa9baa32aa")
+     (220 . "#57809d004c00")
+     (240 . "#48559e556555")
+     (260 . "#392a9faa7eaa")
+     (280 . "#2aa198")
+     (300 . "#28669833af33")
+     (320 . "#279993ccbacc")
+     (340 . "#26cc8f66c666")
+     (360 . "#268bd2")))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   '(unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496"))
+ '(xterm-color-names
+   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"])
+ '(xterm-color-names-bright
+   ["#fdf6e3" "#cb4b16" "#93a1a1" "#839496" "#657b83" "#6c71c4" "#586e75" "#002b36"]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -32,3 +496,10 @@
  ;; If there is more than one, they won't work right.
  '(mode-line ((t (:box nil :weight normal :height 1.0 :width normal :underline nil :overline nil))))
  '(mode-line-inactive ((t (:box nil :weight normal :height 1.0 :width normal :underline nil :overline nil)))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ (setq gc-cons-threshold 16777216
+       gc-cons-percentage 0.1)
