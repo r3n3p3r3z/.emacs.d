657a1f4f7e085d3c05f4f716f742b0f45d221dd7
Alex <alexander.soto@gmail.com>
2019-05-09 13:29:09 -0400

    update

--- core/config-screencast.el
+++ core/config-screencast.el
@@ -1,6 +1,7 @@
 (load-file "~/.emacs.d/.custom/screencast-mode/screencast-mode.el")
 
 (require 'screencast)
+(setq screencast-speed 1.5)
 
 (load-file "~/.emacs.d/screencasts/screencast-outline.el")
 
